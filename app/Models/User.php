b83d7d192ccfe07c8f3c7f905bc2fbc32067bf35
Berenice Lopez Gonzalez <8122110004@utchetumal.edu.mx>
2024-02-29 20:55:32 -0500

    Commit mongo

---
+++ app/Models/User.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use MongoDB\Laravel\Eloquent\Model;
+use MongoDB\Laravel\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    protected $connection = "mongodb";
+    protected $collection = "users_collection";
+    protected $primarykey = "_id";
+
+    public function rol(){
+        return $this->hasMany(Rol::class, 'id_rol','_id');
+    }
+}
+
+
+    
+
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+Route::get('/', function () {
+    return view('app');
+})
+->name('application');
+Route:: get('phpmyinfo', function() {
+    phpinfo();
+    })->name('phpmyinfo');
